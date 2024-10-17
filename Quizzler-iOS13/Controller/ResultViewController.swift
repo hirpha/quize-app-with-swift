9926156fe4fa80b81569cf483ac144ce0ed6f641
Hirpha Fayisa <hirphafayisa88@gmail.com>
2024-10-17 08:43:38 +0300

    Result view controller added

---
+++ Quizzler-iOS13/Controller/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by mac on 10/17/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
